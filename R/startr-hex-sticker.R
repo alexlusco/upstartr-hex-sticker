2ceb5dba7ea37dc300954d7064fec900abedf848
alexlusco <44949344+alexlusco@users.noreply.github.com>
2021-01-15 14:40:25 -0500

    added sparkplug image

--- R/startr-hex-sticker.R
+++ R/startr-hex-sticker.R
@@ -11,37 +11,38 @@
 
 #load packages
 #install.packages("pacman")
-pacman::p_load(hexSticker, here, ggplot2, magick, showtext)
+pacman::p_load(hexSticker, ggplot2, magick, rsvg, showtext)
 
 #add font
 font_add_google("Open Sans")
 
 #read in image
-#img <- image_read("")
+img <- image_read("img/plug.png")
 
 #temporary empty img vector (function doesn't work without an image)
-img <- ggplot() + theme_void()
+#img <- ggplot() + theme_void()
 
 #create and save hex sticker
 s <- sticker(img, 
-        package = "startr", 
-        p_size = 13, #font size
+        package = "upstartr", 
+        p_size = 7, #font size
         p_family = "Open Sans", 
         p_fontface = "bold",
-        p_color = "black",
-        p_x = 1, #font position
-        p_y = 1, #font position
+        p_color = "white",
+        #p_x = 1, #font position
+        #p_y = 1, #font position
         s_x = 1, 
         s_y = .75, 
         s_width = 1.3,
         s_height = .9,
-        h_fill = "#D56161",
-        h_size = 1, #border thickness
-        h_color = "#852E57",
+        h_fill = "#DA161F",
+        h_size = 0, #border thickness
+        #h_color = "#DA161F",
         spotlight = FALSE,
         white_around_sticker = FALSE,
         filename = "hex/startr-hex.png")
+s
 
-#to overwrite with better sizing specs
+#to overwrite with different sizing specs
 #ggsave("hex/startr-hex.png", s, width = 8, height = 8)
                    
